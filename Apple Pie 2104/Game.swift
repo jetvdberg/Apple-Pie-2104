0da8f9c47e5fd06094e33730a8469588c2db7467
jetvdberg <jet.vd.berg@gmail.com>
2017-11-10 13:05:02 +0100

    Apple Pie working app

---
+++ Apple Pie 2104/Game.swift
@@ -0,0 +1,38 @@
+//
+//  Game.swift
+//  Apple Pie 2104
+//
+//  Created by Jet van den Berg on 10-11-17.
+//  Copyright © 2017 Jet van den Berg. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+    
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+}
+
+
+
+
